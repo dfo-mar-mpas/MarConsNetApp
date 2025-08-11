6b6f40216e20a969955ac0600c65d24a5d5c9d9e
Harbin <Jaimie.Harbin@dfo-mpo.gc.ca>
2025-08-11 11:26:49 -0300

    network report

--- sandbox/jh/35_reports/reports.R
+++ sandbox/jh/35_reports/reports.R
@@ -13,3 +13,18 @@ for (i in seq_along(mpas)) {
   output_file <- file.path(paste0(output_dir,"/", make.names(paste0(names=mpas[i], ".html"))))
   render(input=rmd_file, output_file = output_file, output_format = "html_document", params = params, envir = new.env())
 }
+
+
+
+# MARITIMES
+rmd_file <- system.file("data", "network_report.Rmd", package = "MarConsNetApp")
+output_dir <- file.path(Sys.getenv("OneDriveCommercial"),"MarConsNetTargets","data", "reports")
+
+state <- list()
+params <- list()
+input <- list()
+state$mpas <- "Maritimes"
+params$mpas <- "Maritimes"
+input$mpas <- "Maritimes"
+output_file <- file.path(paste0(output_dir,"/", make.names(paste0(names="Maritimes", ".html"))))
+render(input=rmd_file, output_file = output_file, output_format = "html_document", params = params, envir = new.env())
