b1ef0583c7e685fcd6a0b1765739832a7b887de0
Harbin <Jaimie.Harbin@dfo-mpo.gc.ca>
2025-09-22 15:27:13 -0300

    network and site reports

--- sandbox/jh/35_reports/reports.R
+++ sandbox/jh/35_reports/reports.R
@@ -48,10 +48,12 @@ render(input=rmd_file, output_file = output_file, output_format = "html_document
 
 
 
-# ACTUAL
+# ACTUAL network
 
-render(input="C:/Users/HARBINJ/Documents/GitHub/MarConsNetApp/data/report.Rmd", output_file = "C:/Users/HARBINJ/Documents/REPORT_TESTS//NEW_ACTUAL.html", output_format = "html_document", params = params)
+render(input="C:/Users/HARBINJ/Documents/GitHub/MarConsNetApp/data/network_report.Rmd", output_file = "C:/Users/HARBINJ/Documents/REPORT_TESTS//NEW_NETWORK.html", output_format = "html_document", params = params)
 
+# SITE
+render(input="C:/Users/HARBINJ/Documents/GitHub/MarConsNetApp/data/report.Rmd", output_file = "C:/Users/HARBINJ/Documents/REPORT_TESTS//NEW_SITE.html", output_format = "html_document", params = params)
 
 
 
