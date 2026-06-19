c977b9cf029de8715c954b12a803d9d3c6a262b5
hallepayton <halle.payton0@gmail.com>
2026-06-19 15:27:22 -0300

    Flow chart updates

--- sandbox/hm/02_data_ingestion/flow_chart.R
+++ sandbox/hm/02_data_ingestion/flow_chart.R
@@ -1,10 +1,11 @@
 
 library(DiagrammeR)
 
+
 grViz("
 digraph process {
 
-  graph [rankdir = TB]
+  graph [rankdir = TB, nodesep = 0.6]
 
   node [
   fontname = Helvetica,
@@ -20,41 +21,54 @@ edge [
 ]
 
 Note1 [
-  label = 'Power Automate Flow 1 pulls responses\\nfrom initial form responses to create\\ndataset record',
+  label = 'Power Automate Flow 1 sends an email\\n with an initial interest form once an SME\\nrecord is created in List 1',
   shape = note,
   style = filled,
 
 ]
-
 
 Note2 [
-  label = 'Power Automate Flow 2 sends an email\\nwith the data collection form once\\nsprint start date is entered',
+  label = 'Power Automate Flow 2 pulls responses\\nfrom initial form responses to create\\ndataset record',
   shape = note,
   style = filled,
 
 ]
+
 
 Note3 [
-  label = 'Power Automate Flow 3 pulls responses from\\ndata collection form to fill dataset record',
+  label = 'Power Automate Flow 3 sends an email\\nwith the data collection form once\\nsprint start date is entered in List 2',
   shape = note,
   style = filled,
 
 ]
+
+Note4 [
+  label = 'Power Automate Flow 4 pulls responses from\\ndata collection form to fill dataset record',
+  shape = note,
+  style = filled,
+
+]
+
 {
   rank = same
   Note1
+  InitialEmail
+}
+{
+  rank = same
+  Note2
   DatasetRecord
 }
 
 {
   rank = same
-  Note2
+  Note3
   SprintStart
 }
 
 {
   rank = same
-  Note3
+  Note4
   CompleteRecord
 }
 
@@ -81,6 +95,7 @@ Note3 [
   FinalReview     [label = 'Final Review\\nwith SME', shape = oval, style = 'rounded, filled', fillcolor = palegreen]
 
   CoffeeChat -> InitialEmail
+  InitialEmail -> Note1 [style = dashed, arrowhead = none, constraint = false]
   InitialEmail -> InterestForm
 
   InterestForm -> FollowUp1 [label = 'No']
@@ -88,20 +103,20 @@ Note3 [
 
   InterestForm -> DatasetRecord [label = 'Yes']
 
-  DatasetRecord -> Note1  [style = dashed, arrowhead = none, constraint = false]
+  DatasetRecord -> Note2  [style = dashed, arrowhead = none, constraint = false]
   DatasetRecord -> SMEAvailable
 
   SMEAvailable -> ScheduleLater [label = 'No']
   SMEAvailable -> SprintStart [label = 'Yes']
 
-  SprintStart -> Note2 [style = dashed, arrowhead = none, constraint = false]
+  SprintStart -> Note3 [style = dashed, arrowhead = none, constraint = false]
   SprintStart -> CollectionEmail
   CollectionEmail -> DataForm
 
   DataForm -> FollowUp2 [label = 'No']
   DataForm -> CompleteRecord [label = 'Yes']
 
-  CompleteRecord -> Note3 [style = dashed, arrowhead = none, constraint = false]
+  CompleteRecord -> Note4 [style = dashed, arrowhead = none, constraint = false]
   CompleteRecord -> DataInspection
   DataInspection -> DataAccess
 
