5ae51b28bdfc1bb933047c33c931a9d5722ea4fa
hallepayton <halle.payton0@gmail.com>
2026-07-24 14:52:00 -0300

    Kelp updates

--- sandbox/hm/02_data_ingestion/Kelp/kelp_onedrive.R
+++ sandbox/hm/02_data_ingestion/Kelp/kelp_onedrive.R
@@ -215,3 +215,57 @@ percent_change_sl <- cur_site_summary_sl %>%
       ) * 100
     )
   )
+
+
+
+##PROCESS INDICATOR
+
+data <-
+
+
+  data$year_of_publication <- {
+
+    url <- "https://open.canada.ca/data/en/dataset/f1a022a4-b9bf-47d0-b641-2067ea568962/resource/a47f5b93-9424-48a5-a65c-5fd5c6bc2b70"
+    page <- read_html(url)
+    page_text <- html_text2(page)
+
+    page_text |>
+      str_extract("(?<=Data last updated )\\w+ \\d{1,2}, \\d{4}") |>
+      str_extract("\\d{4}")
+  }
+
+
+x <- process_indicator(
+  data = data,
+  indicator_var_name = "ind_distinctive_benthic_characteristics",      ##This would replace the St Anns Bank placholder indicator
+  indicator = "Diversity and community composition of the benthos and characteristics of surficial geology at selected sampling stations located in the identified distinctive seabed features of the AOI, plus abundance or biomass and size composition of the defining benthic taxa of those features",
+  type = "model",
+  units = " ",
+  scoring = "representation",
+  PPTID = c(1633, 2576),
+  source = "",
+  project_short_title = "Species distribution models of kelp and non-indigenous macroalgae in the DFO Maritimes region",
+  areas = MPAs,
+  climate_expectation = "FIXME",
+  indicator_rationale = "Kelp forests support high biodiversity and productivity, and provide ecosystem services",
+  bin_rationale = "FIXME",
+  plot_type = c("time-series", "map"),
+  year = 'year_of_data_collection',
+  objectives = c(
+    "Protect unique, rare, or sensitive ecological features",
+    "Conserve and protect all major benthic, demersal (i.e., close to the sea floor) and pelagic (i.e., in the water column) habitats within the MPA, along with their associated physical, chemical, geological and biological properties and processes",
+    "Habitat required for all species, particularly priority species, is maintained and protected"
+  ),
+  theme = "Benthic Environment",
+  SME = "Unknown",
+  control_polygon = control_polygons,
+  plot_lm = FALSE
+)
+
+save_plots(dplyr::select(x, -data, -adjacent_data))
+dplyr::select(x, -plot)
+
+
+
+
+
