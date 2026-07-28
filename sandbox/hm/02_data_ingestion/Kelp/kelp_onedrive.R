433ff133d9f2f52488f155ab7b3e329db0baa80c
hallepayton <halle.payton0@gmail.com>
2026-07-28 12:32:59 -0300

    Kelp Updates

--- sandbox/hm/02_data_ingestion/Kelp/kelp_onedrive.R
+++ sandbox/hm/02_data_ingestion/Kelp/kelp_onedrive.R
@@ -1,3 +1,168 @@
+
+
+#----------------------------------
+#  INDICATOR 1
+#----------------------------------
+
+tar_target(name = ind_distinctive_benthic_characteristics, command = {
+  onedrive <- Sys.getenv("OneDriveCommercial")
+
+  raster_folder <- file.path(
+    onedrive,
+    "Krumhansl, Kira (DFO_MPO)'s files - 2021 2024 Species Distribution Model Outputs"
+  )
+
+  l_digitata_current <- rast(
+    file.path(
+      raster_folder,
+      "Laminaria digitata",
+      "Laminaria_digitata_Bathy_rm4_20240223_avg_Binary.tif"
+    )
+  )
+
+  s_latissima_current <- rast(
+    file.path(
+      raster_folder,
+      "Saccharina latissima",
+      "Saccharina_latissima_Bathy_rm2MinusRugosityAndProfile_20240223_avg_Binary.tif"
+    )
+  )
+
+  url <- paste0(
+    "https://egisp.dfo-mpo.gc.ca/arcgis/rest/services/",
+    "open_data_donnees_ouvertes/draft_conservation_network_sites/",
+    "MapServer/0/query?",
+    "where=1=1&outFields=*&f=geojson"
+  )
+
+  conservation_network <- st_read(url)
+  sites <- vect(conservation_network)
+
+  extract_site_summary <- function(raster, sites, raster_column, output_name) {
+
+    sites_proj <- project(sites, crs(raster))
+    values <- terra::extract(raster, sites_proj)
+    values$SiteName_E <- sites_proj$SiteName_E[values$ID]
+
+    summary <- values %>%
+      group_by(SiteName_E) %>%
+      summarise(
+        !!output_name := mean(.data[[raster_column]], na.rm = TRUE) * 100
+      )
+
+    return(summary)
+  }
+
+  cur_ld <- extract_site_summary(
+    l_digitata_current,
+    sites,
+    "Laminaria_digitata_Bathy_rm4_20240223_avg_Binary",
+    "cur_occurrence_percent_ld"
+  )
+
+  cur_sl <- extract_site_summary(
+    s_latissima_current,
+    sites,
+    "Saccharina_latissima_Bathy_rm2MinusRugosityAndProfile_20240223_avg_Binary",
+    "cur_occurrence_percent_sl"
+  )
+
+  data <- cur_sl %>%
+    left_join(
+      cur_ld,
+      by = "SiteName_E"
+    )
+
+
+  data$year_of_publication <- {
+
+    ld_path <- file.path(
+      onedrive,
+      "Krumhansl, Kira (DFO_MPO)'s files - 2021 2024 Species Distribution Model Outputs",
+      "Laminaria digitata",
+      "Laminaria_digitata_Bathy_rm4_20240223_avg_Binary.tif"
+    )
+
+    sl_path <- file.path(
+      onedrive,
+      "Krumhansl, Kira (DFO_MPO)'s files - 2021 2024 Species Distribution Model Outputs",
+      "Saccharina latissima",
+      "Saccharina_latissima_Bathy_rm2MinusRugosityAndProfile_20240223_avg_Binary.tif"
+    )
+
+    # Get modified dates
+    file_dates <- file.info(c(ld_path, sl_path))$mtime
+    format(max(file_dates), "%Y")
+  }
+
+  x <- process_indicator(
+    data = data,
+    indicator_var_name = "ind_distinctive_benthic_characteristics",      ##This would replace the St Anns Bank placholder indicator
+    indicator = "Diversity and community composition of the benthos and characteristics of surficial geology at selected sampling stations located in the identified distinctive seabed features of the AOI, plus abundance or biomass and size composition of the defining benthic taxa of those features",
+    type = "model",
+    units = " ",
+    scoring = "coverage",
+    PPTID = NA,
+    source = "OneDrive",
+    project_short_title = "Species distribution models of kelp and non-indigenous macroalgae in the DFO Maritimes region",
+    areas = MPAs,
+    climate_expectation = "FIXME",
+    indicator_rationale = "Kelp forests support high biodiversity and productivity, and provide ecosystem services",
+    bin_rationale = "FIXME",
+    plot_type = "map",
+    year = 'year_of_data_collection',
+    objectives = c(
+      "Protect unique, rare, or sensitive ecological features",
+      "Conserve and protect all major benthic, demersal (i.e., close to the sea floor) and pelagic (i.e., in the water column) habitats within the MPA, along with their associated physical, chemical, geological and biological properties and processes",
+      "Habitat required for all species, particularly priority species, is maintained and protected"
+    ),
+    theme = "Benthic Environment",
+    SME = "Unknown",
+    control_polygon = control_polygons,
+    plot_lm = FALSE
+  )
+
+  save_plots(dplyr::select(x, -data, -adjacent_data))
+  dplyr::select(x, -plot)
+
+
+
+  ## PLOTS
+
+
+  levels(s_latissima_current) <- data.frame(
+    value = c(0, 1),
+    occurrence = c("Non-occurrence", "Predicted occurrence")
+  )
+
+  terra::plot(
+    s_latissima_current,
+    main = "Predicted Occurence of Saccharina latissima",
+    xlab = "Longitude",
+    ylab = "Latitude",
+    col = c("white", "purple"),
+    axes = TRUE,
+    legend = TRUE
+  )
+
+  plot(
+    sites,
+    add = TRUE,
+    col = NA,
+    border = "black",
+    lwd = 1.5
+  )
+
+})
+
+
+
+#-----------------------------
+# PRACTICE
+#-----------------------------
+
+
+
 library(terra)
 
 ##get the data
@@ -57,19 +222,19 @@ sites <- vect(conservation_network)
 # CURRENT PREDICTED OCCURRENCE L. DIGITATA
 
 ##Ensure the same coordinate system
-cur_sites_ld <- project(sites, crs(l_digitata_current))
+cur_ld <- project(sites, crs(l_digitata_current))
 
 #get all raster values in each CS
-cur_site_values_ld <- extract(
+cur_values_ld <- terra::extract(
   l_digitata_current,
-  cur_sites_ld
+  cur_ld
 )
 
 # Add the corresponding site names to the extracted values
-cur_site_values_ld$SiteName_E <- cur_sites_ld$SiteName_E[cur_site_values_ld$ID]
+cur_values_ld$SiteName_E <- cur_ld$SiteName_E[cur_values_ld$ID]
 
 ##calculates the summary stats for each site
-cur_site_summary_ld <- cur_site_values_ld %>%
+cur_summary_ld <- cur_values_ld %>%
   group_by(SiteName_E) %>%
   summarise(
     cur_occurrence_cells_ld = sum(
@@ -87,16 +252,16 @@ cur_site_summary_ld <- cur_site_values_ld %>%
 
 ## FUTURE PREDICTED OCCURRENCE L. DIGITATA
 
-fut_sites_ld <- project(sites, crs(l_digitata_future))
+fut_ld <- project(sites, crs(l_digitata_future))
 
-fut_site_values_ld <- extract(
+fut_values_ld <- terra::extract(
   l_digitata_future,
   sites
 )
 
-fut_site_values_ld$SiteName_E <- fut_sites_ld$SiteName_E[fut_site_values_ld$ID]
+fut_values_ld$SiteName_E <- fut_ld$SiteName_E[fut_values_ld$ID]
 
-fut_site_summary_ld <- fut_site_values_ld %>%
+fut_summary_ld <- fut_values_ld %>%
   group_by(SiteName_E) %>%
   summarise(
     fut_occurrence_cells_ld = sum(
@@ -113,13 +278,13 @@ fut_site_summary_ld <- fut_site_values_ld %>%
   )
 
 ###PERCENT CHANGE OVER TIME L. DIGITATA
-percent_change_ld <- cur_site_summary_ld %>%
+percent_change_ld <- cur_summary_ld %>%
   select(
     SiteName_E,
     cur_occurrence_percent_ld
   ) %>%
   left_join(
-    fut_site_summary_ld %>%
+    fut_summary_ld %>%
       select(
         SiteName_E,
         fut_occurrence_percent_ld
@@ -138,17 +303,18 @@ percent_change_ld <- cur_site_summary_ld %>%
   )
 
 # CURRENT PREDICTED OCCURRENCE S. LATISSIMA
-cur_sites_sl <- project(sites, crs(s_latissima_current))
+cur_sl <- project(sites, crs(s_latissima_current))
 
-cur_site_values_sl <- extract(
+cur_values_sl <- terra::extract(
   s_latissima_current,
-  cur_sites_sl
+  cur_sl
 )
 
-cur_site_values_sl$SiteName_E <- cur_sites_sl$SiteName_E[cur_site_values_sl$ID]
+cur_values_sl$SiteName_E <- cur_sl$SiteName_E[cur_values_sl$ID]
 
+options(scipen = 999)
 
-cur_site_summary_sl <- cur_site_values_sl %>%
+cur_summary_sl <- cur_values_sl %>%
   group_by(SiteName_E) %>%
   summarise(
     cur_occurrence_cells_sl = sum(
@@ -166,16 +332,16 @@ cur_site_summary_sl <- cur_site_values_sl %>%
 
 
 ## FUTURE PREDICTED OCCURRENCE S. LATISSIMA
-fut_sites_sl <- project(sites, crs(s_latissima_future))
+fut_sl <- project(sites, crs(s_latissima_future))
 
-fut_site_values_sl <- extract(
+fut_values_sl <- terra::extract(
   s_latissima_future,
   sites
 )
 
-fut_site_values_sl$SiteName_E <- fut_sites_sl$SiteName_E[fut_site_values_sl$ID]
+fut_values_sl$SiteName_E <- fut_sl$SiteName_E[fut_values_sl$ID]
 
-fut_site_summary_sl <- fut_site_values_sl %>%
+fut_summary_sl <- fut_values_sl %>%
   group_by(SiteName_E) %>%
   summarise(
     fut_occurrence_cells_sl = sum(
@@ -192,7 +358,7 @@ fut_site_summary_sl <- fut_site_values_sl %>%
   )
 
 ###PERCENT CHANGE OVER TIME S. LATISSIMA
-percent_change_sl <- cur_site_summary_sl %>%
+percent_change_sl <- cur_summary_sl %>%
   select(
     SiteName_E,
     cur_occurrence_percent_sl
@@ -217,53 +383,28 @@ percent_change_sl <- cur_site_summary_sl %>%
   )
 
 
-
-##PROCESS INDICATOR
-
-data <-
-
-
-  data$year_of_publication <- {
-
-    url <- "https://open.canada.ca/data/en/dataset/f1a022a4-b9bf-47d0-b641-2067ea568962/resource/a47f5b93-9424-48a5-a65c-5fd5c6bc2b70"
-    page <- read_html(url)
-    page_text <- html_text2(page)
-
-    page_text |>
-      str_extract("(?<=Data last updated )\\w+ \\d{1,2}, \\d{4}") |>
-      str_extract("\\d{4}")
-  }
+combined_summary <- cur_summary_sl %>%
+  dplyr::select(SiteName_E, cur_occurrence_percent_sl) %>%
+  left_join(
+    cur_summary_ld %>%
+      dplyr::select(SiteName_E, cur_occurrence_percent_ld),
+    by = "SiteName_E"
+  )
 
 
-x <- process_indicator(
-  data = data,
-  indicator_var_name = "ind_distinctive_benthic_characteristics",      ##This would replace the St Anns Bank placholder indicator
-  indicator = "Diversity and community composition of the benthos and characteristics of surficial geology at selected sampling stations located in the identified distinctive seabed features of the AOI, plus abundance or biomass and size composition of the defining benthic taxa of those features",
-  type = "model",
-  units = " ",
-  scoring = "representation",
-  PPTID = c(1633, 2576),
-  source = "",
-  project_short_title = "Species distribution models of kelp and non-indigenous macroalgae in the DFO Maritimes region",
-  areas = MPAs,
-  climate_expectation = "FIXME",
-  indicator_rationale = "Kelp forests support high biodiversity and productivity, and provide ecosystem services",
-  bin_rationale = "FIXME",
-  plot_type = c("time-series", "map"),
-  year = 'year_of_data_collection',
-  objectives = c(
-    "Protect unique, rare, or sensitive ecological features",
-    "Conserve and protect all major benthic, demersal (i.e., close to the sea floor) and pelagic (i.e., in the water column) habitats within the MPA, along with their associated physical, chemical, geological and biological properties and processes",
-    "Habitat required for all species, particularly priority species, is maintained and protected"
-  ),
-  theme = "Benthic Environment",
-  SME = "Unknown",
-  control_polygon = control_polygons,
-  plot_lm = FALSE
-)
 
-save_plots(dplyr::select(x, -data, -adjacent_data))
-dplyr::select(x, -plot)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
